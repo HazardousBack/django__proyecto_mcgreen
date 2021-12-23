1ac8661ece12ad9f77a492d4744fd680b6791cca
Erick Patiño <erickfco1999@gmail.com>
2021-12-22 22:20:01 -0600

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth.models import User
 from .models import Puestos, Privilegios
 
 ELEGIR_ROL = {
@@ -82,15 +81,3 @@ class Formulario_registro(forms.Form):
     rol = forms.ChoiceField(label="Rol", choices=ELEGIR_ROL, widget=forms.RadioSelect(attrs={
         'class': 'form-check-input'
     }))
-
-# class PrettyAuthenticationForm(AuthenticationForm):
-#     def __init__(self, *args, **kwargs):
-#         super(PrettyAuthenticationForm, self).__init__(*args, **kwargs)
-#     username = forms.EmailField(widget=forms.TextInput(
-#     attrs={'class': 'form-control', 'placeholder': 'Username', 'id': 'hello'}))
-#     password = forms.CharField(widget=forms.PasswordInput(
-#         attrs={
-#             'class': 'form-control',
-#             'placeholder': 'Password',
-#             'id': 'hi',
-#         }))
