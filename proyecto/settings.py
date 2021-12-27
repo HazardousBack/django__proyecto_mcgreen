0f84bc643be9f2e5e00b82b0c931b09e5e7c3a6b
Erick <erickfco1999m@gmail.com>
2021-12-27 13:08:29 -0600

    Debug encendido

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY",'ow7n7$+88*2vv2sf+2)l2#699fkt5qu#$^)q*&wj8vafqibn!i')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 0
+DEBUG = str(os.environ.get("DEBUG")) == "1"
 
 ENV_ALLOWED_HOST = os.environ.get("DJANGO_ALLOWED_HOST") or None
 ALLOWED_HOSTS = []
