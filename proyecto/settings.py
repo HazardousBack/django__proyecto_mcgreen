d85f9406c8017ca01643b985c92c6898019a530d
Erick <erickfco1999m@gmail.com>
2021-12-28 12:52:39 -0600

    mime types

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+import mimetypes
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+mimetypes.add_type("text/css", ".css", True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
